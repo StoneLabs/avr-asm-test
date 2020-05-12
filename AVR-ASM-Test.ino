599a3c9c431736bc8277bbdac34a0778205e6429
StoneLabs <levyehrstein@googlemail.com>
2020-05-12 12:59:06 +0200

    Added working code

---
+++ AVR-ASM-Test.ino
@@ -0,0 +1,22 @@
+﻿#include <Arduino.h>
+
+extern "C" {
+	// function prototypes
+	void start();
+	void blink();
+}
+
+int main(void)
+{
+	init();
+
+	Serial.begin(9600);	delay(250);
+	Serial.println("Welcome!");
+
+	start();
+
+	while (true)
+		blink();
+
+	return 0;
+}
